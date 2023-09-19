f4c3486b7c8fb709ad23bed16d8d2ec4ec2ee2ed
Sanskar Tiwari <sanskar.mk2@gmail.com>
2023-09-19 14:24:09 +0530

    initial

---
+++ Shorts Be Gone.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         Shorts Be Gone
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  stop google from making everyone a pedophile by pushing little girls twerking 30 seconds videos.
+// @author       Lightheart
+// @match        https://www.youtube.com/*
+// @icon         https://www.youtube.com/s/desktop/dbf5c200/img/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    var observer;
+
+    function removeShorts() {
+        var aTags = document.querySelectorAll('a');
+        var shelfList = document.querySelectorAll('ytd-reel-shelf-renderer.style-scope.ytd-item-section-renderer');
+
+        for (let shelf of shelfList) {
+            shelf.remove();
+        }
+
+        for (var i = 0; i < aTags.length; i++) {
+            if (aTags[i].href.startsWith("https://www.youtube.com/shorts")) {
+                var element = aTags[i];
+
+                while (element.parentElement && (element.className != 'style-scope ytd-rich-grid-renderer' || element.nodeName != 'YTD-RICH-SECTION-RENDERER')) {
+                    element = element.parentElement;
+                }
+
+                if (element.className == 'style-scope ytd-rich-grid-renderer' && element.nodeName == 'YTD-RICH-SECTION-RENDERER') {
+                    element.remove();
+                }
+            } else if (aTags[i].title === "Shorts") {
+                aTags[i].parentElement.remove();
+            }
+        }
+
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+
+    var callback = function (mutationsList) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                observer.disconnect();
+                requestAnimationFrame(removeShorts);
+            }
+        }
+    }
+
+    observer = new MutationObserver(callback);
+
+    setInterval(function () { requestAnimationFrame(removeShorts); }, 1000);
+
+})();
